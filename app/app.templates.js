90046f1ba457a8f573d153409b710bbef0108299
Jeremy Letto <jletto87@gmail.com>
2016-04-07 22:33:35 -0400

    animation

--- app/app.templates.js
+++ app/app.templates.js
@@ -2,7 +2,12 @@ angular.module('app').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('app/dashboard/dashboard.template.html',
-    "<div class=home-page><h1>{{config.site}}</h1></div>"
+    "<div class=home-page><div class=hero><h1>{{config.site}}</h1></div></div>"
+  );
+
+
+  $templateCache.put('app/initializer/initializer.template.html',
+    "<div class=intializer-page><div class=hero><h1 class=rotater>{{config.site}}</h1></div></div>"
   );
 
 }]);
