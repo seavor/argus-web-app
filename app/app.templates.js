d9da8eab4168e85bc756abd1781c6d3382fd5321
Jeremy Letto <jletto87@gmail.com>
2016-04-09 11:22:03 -0400

    extend styling for dashboard

--- app/app.templates.js
+++ app/app.templates.js
@@ -2,7 +2,7 @@ angular.module('app').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('app/dashboard/dashboard.template.html',
-    "<div class=home-page><div class=home-page-header><a id=siteID ui-sref=dashboard>the argus project</a> <span class=home-page-header-byline>THE WHOLE WORLD IS WATCHING</span><p class=home-page-header-tagline>Counter-surveillance Armor for the Citizen Body</p></div><div class=home-page-content><div class=body-suit><div class=body-suit-wrapper><div class=body-suit-hud></div><body-suit></body-suit></div><div class=body-suit-animation></div></div><div class=video-feed><div class=video-feed-wrapper><div class=video-feed-hud></div><video-feed></video-feed></div><div class=video-feed-animation></div><div class=video-feed-thumbnails></div></div></div></div>"
+    "<div class=home-page><div class=home-page-header><a id=siteID ui-sref=dashboard>the argus project</a> <span class=home-page-header-byline>THE WHOLE WORLD IS WATCHING</span><p class=home-page-header-tagline>Counter-surveillance Armor for the Citizen Body</p></div><div class=home-page-content><div class=body-suit><div class=body-suit-wrapper><div class=body-suit-hud></div><body-suit class=body-suit-directive></body-suit></div><div class=body-suit-animation></div></div><div class=video-feed><div class=video-feed-wrapper><div class=video-feed-hud></div><video-feed class=video-feed-directive></video-feed></div><div class=video-feed-animation></div><div class=video-feed-thumbnails></div></div></div></div>"
   );
 
 
