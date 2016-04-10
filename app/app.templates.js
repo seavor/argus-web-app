9bec2f2ea8f9b2a7228c1716d57276c7e0624cc3
Jeremy Letto <jletto87@gmail.com>
2016-04-10 02:01:37 -0400

    wire up view routing

--- app/app.templates.js
+++ app/app.templates.js
@@ -7,12 +7,17 @@ angular.module('app').run(['$templateCache', function($templateCache) {
     "        height: 75%;\n" +
     "        top: 14%;\n" +
     "        left: 17%;\n" +
-    "      '\"><body-suit></body-suit></hud-element><hud-animation class=body-suit-animation src=\"'images/bottom-hud.svg'\"></hud-animation></div><div class=video-feed><hud-element class=video-feed-directive border=\"'http://placehold.it/1600x900'\" cover></hud-element><hud-animation class=video-feed-animation src=\"'images/right-hud.png'\"></hud-animation><hud-element class=video-feed-thumbnails border=\"'images/borders/thumbnails.png'\" content-position=\"'\n" +
+    "      '\"><body-suit></body-suit></hud-element><hud-animation class=body-suit-animation src=\"'images/bottom-hud.svg'\"></hud-animation></div><div class=video-feed><hud-element class=video-feed-directive border=\"'http://placehold.it/1600x900'\" cover>{{selectedFeed.label}}</hud-element><hud-animation class=video-feed-animation src=\"'images/right-hud.png'\"></hud-animation><hud-element class=video-feed-thumbnails border=\"'images/borders/thumbnails.png'\" content-position=\"'\n" +
     "        width: 85%;\n" +
     "        height: 50%;\n" +
     "        top: 20%;\n" +
     "        left: 1%;\n" +
-    "      '\"><div class=thumbnails><div class=thumbnails-track><hud-element class=thumbnails-item border=\"'images/borders/thumbnail.png'\" ng-repeat=\"thumbnail in thumbnails\"></hud-element></div></div></hud-element></div></div></div>"
+    "      '\"><div class=thumbnails><div class=thumbnails-track><hud-element cover=true class=thumbnails-item border=\"'images/borders/thumbnail.png'\" ng-repeat=\"thumbnail in thumbnails\" ng-click=viewFeed($index) content-position=\"'\n" +
+    "                width: 94%;\n" +
+    "                height: 86%;\n" +
+    "                top: 5%;\n" +
+    "                left: 2%;\n" +
+    "              '\"><div class=thumbnails-item-image style=\"background-image: url(images/thumbnail.png)\"></div></hud-element></div></div></hud-element></div></div></div>"
   );
 
 
--- js/dashboard/dashboard.controller.js
+++ js/dashboard/dashboard.controller.js
@@ -1,13 +1,38 @@
 (function() {
-    angular.module("app").controller("DashboardCtrl", [ "config", "$scope", "$state", "apngSrvc", "localStorageService", function(config, $scope, $state, apngSrvc, localStorageService) {
+    angular.module("app").controller("DashboardCtrl", [ "$scope", "$timeout", "$state", "apngSrvc", "localStorageService", function($scope, $timeout, $state, apngSrvc, localStorageService) {
         console.info("Initializing Dashboard Controller: ", $scope);
         $scope.thumbnails = [ {
             id: 1,
+            label: "head",
+            src: "cam-thumb.png"
+        }, {
+            id: 2,
+            label: "right arm",
+            src: "cam-thumb.png"
+        }, {
+            id: 3,
+            label: "left arm",
+            src: "cam-thumb.png"
+        }, {
+            id: 4,
             label: "right leg",
             src: "cam-thumb.png"
+        }, {
+            id: 5,
+            label: "left leg",
+            src: "cam-thumb.png"
         } ];
+        $scope.selectedFeed = $scope.thumbnails[localStorageService.get("selectedIdx") || 0];
+        $scope.viewFeed = viewFeed;
         apngSrvc.cacheAssets().then(function() {
             $scope.assetsLoaded = true;
         });
+        function viewFeed(idx) {
+            $timeout(function() {
+                console.log("View Feed: ", idx);
+                localStorageService.set("selectedIdx", idx);
+                $scope.selectedFeed = $scope.thumbnails[idx];
+            });
+        }
     } ]);
 })();
